5b9d68c15f47d845ed8e66ea00ed3d3617401041
son123j <49780223+son123j@users.noreply.github.com>
2021-02-16 21:38:32 +0900

    Add files via upload

---
+++ Plugin_test/src/main/java/com/Plugin_test/plugin/Main.java
@@ -0,0 +1,17 @@
+package com.Plugin_test.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("플러그인 활성화");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
